7a1286f5c169160306599d7ef10fe9e30489a71d
EkaterinaZhukova <kate.zhukova.98@gmail.com>
2019-10-02 01:11:01 +0300

    changed on tuple

--- dictionary.hs
+++ dictionary.hs
@@ -1,18 +1,17 @@
--- data Dictionaty = Empty 
--- 	| Nonemty 
+import Data.Tuple
 
-data Map a = Empty | NonEmpty a a (Map a)
+data Map a = Empty | NonEmpty ((,) a a)  (Map a)
 
 
 printMap :: Show t => Map t -> String
 printMap Empty = ""
-printMap (NonEmpty key val nextkey) = show key ++ " - " ++ show val ++ "; " ++ printMap nextkey
+printMap (NonEmpty tuple nextkey) = show (fst tuple) ++ " - " ++ show (snd tuple) ++ "; " ++ printMap nextkey
 
 
-insert :: Ord d => (Map d) -> d -> d -> (Map d)
-insert Empty key val = NonEmpty key val Empty
-insert p@(NonEmpty key_p val_p nextItem) key val
-	| key_p == key		= p
-	| otherwise			= NonEmpty key_p val_p (insert nextItem key val)
+insert :: Ord d => (Map d) -> ((,) d d) -> (Map d)
+insert Empty tuple = NonEmpty tuple Empty
+insert p@(NonEmpty tuple nextItem) tupleToInsert
+	| (fst tuple) == (fst tupleToInsert)		= p
+	| otherwise									= NonEmpty tuple (insert nextItem tupleToInsert)
 
 
